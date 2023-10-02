ad07786c07c881ae5f0b723cf7d66e0ac968f380
Guerra-BR <guerinohenrique@gmail.com>
2023-10-02 16:23:15 -0300

    carrinhl reducer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,21 @@
 import * as S from './styles'
 
 import { Produto } from '../../App'
+import { RootReducer } from '../../store'
 
 import cesta from '../../assets/cesta.png'
 import { paraReal } from '../Produto'
+import { useSelector } from 'react-redux'
 
 type Props = {
-  itensNoCarrinho: Produto[]
   favoritos: Produto[]
 }
 
-const Header = ({ itensNoCarrinho, favoritos }: Props) => {
+const Header = ({ favoritos }: Props) => {
+  const itensNoCarrinho = useSelector(
+    (state: RootReducer) => state.carrinho.itens
+  )
+
   const valorTotal = itensNoCarrinho.reduce((acc, item) => {
     acc += item.preco
     return acc
